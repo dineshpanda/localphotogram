9d9efa9aef87e6a762f2d530cda681627632b068
Dinesh Panda <dineshpanda92@gmail.com>
2020-08-21 18:21:24 +0530

    3rd draft

--- db/migrate/20200821121910_add_owner_reference_to_photos.rb
+++
@@ -1,4 +0,0 @@
-class AddOwnerReferenceToPhotos < ActiveRecord::Migration[6.0]
-  def change    add_foreign_key :photos, :users, column: :owner_id    add_index :photos, :owner_id
-    change_column_null :photos, :owner_id, false  end
-end
