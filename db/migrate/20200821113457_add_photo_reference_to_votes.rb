630ff4f70dd00fe06c5eb8c1e76e31c93a5dfe11
Dinesh Panda <dineshpanda92@gmail.com>
2020-08-21 17:51:45 +0530

    2nd draft

--- db/migrate/20200821113457_add_photo_reference_to_votes.rb
+++
@@ -1,5 +0,0 @@
-class AddPhotoReferenceToVotes < ActiveRecord::Migration[6.0]
-  def change    add_foreign_key :votes, :photos, column: :photo_id    add_index :votes, :photo_id    change_column_null :votes, :photo_id, false
-    add_reference :votes, :photo, null: false, foreign_key: true
-  end
-end
